3333088c6186cd512e8a06fee44f30cded3c7edb
Colleen Geohagan <colleen.geohagan@voxmedia.com>
2020-10-06 17:32:01 -0400

    updating to ejs

--- intro/app.js
+++ intro/app.js
@@ -2,36 +2,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 
 // NPM modules
 const app = express();
 
-// layoutsDir is default so redundant
-app.engine('hbs', expressHbs({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs'
-}));
-// Registering templating engine that's not built in
-// returns initialized view engine
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 // This is the default so it's unnecessary, but showing for clarity
 app.set('views', 'views');
 
 // Separate files
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // can use routes like normal middleware
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '404' });
 });
 
 app.listen(3000);
--- intro/util/path.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-// gives us the path to the main module that started the application (i.e., app.js )
-// we are getting the path to that directory
-module.exports = path.dirname(process.mainModule.filename);
