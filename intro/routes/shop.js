92acfd9babd5ddebee6aa4c922364ad04380b40c
Colleen Geohagan <colleen.geohagan@voxmedia.com>
2020-10-15 11:32:16 -0400

    updating cart, deleting products from cart

--- intro/routes/shop.js
+++ intro/routes/shop.js
@@ -10,7 +10,11 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
+router.post('/cart', shopController.postCart);
+
 router.get('/cart', shopController.getCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.get('/orders', shopController.getOrders);
 
